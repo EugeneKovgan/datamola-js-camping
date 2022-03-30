1c40e2cd1e00308d99ecdbdfc1fa24e91b942c7e
Eugene Kovgan <jugin.kovgan87@gmail.com>
2022-03-30 15:19:13 +0300

    fix: fix after check extra-task 4.1

--- JS-Deep-Dive-and-Best-Practices/extra tasks/task_1/main.js
+++ JS-Deep-Dive-and-Best-Practices/extra tasks/task_1/main.js
@@ -8,23 +8,76 @@ class Node {
     return `${this.value}`;
   }
 }
-
 class List {
   constructor() {
     this.head = null;
-    this.tail = null;
+    this.size = 0;
   }
 
-  addNode(value) {
-    const newNode = new Node(value);
-    if (!this.head || !this.tail) {
-      this.head = newNode;
-      this.tail = newNode;
-      return this;
+  insertFirst(value) {
+    this.head = new Node(value, this.head);
+    this.size++;
+  }
+
+  insertLast(value) {
+    let node = new Node(value);
+    let current;
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
     }
-    this.tail.next = newNode;
-    this.tail = newNode;
-    return this;
+    this.size++;
+  }
+
+  addNode(value, index) {
+    if (index > 0 && index > this.size) {
+      return false;
+    }
+    if (index === 0) {
+      this.head = new Node(value, this.head);
+      return;
+    }
+    if (index) {
+      const node = new Node(value);
+      let current;
+      let previous;
+      current = this.head;
+      let count = 0;
+      while (count < index) {
+        previous = current; // нода перед индексом
+        count++;
+        current = current.next; // нода после индекса
+      }
+      node.next = current;
+      previous.next = node;
+      this.size++;
+    }
+    this.insertLast(value);
+  }
+
+  removeNode(index) {
+    if (index > 0 && index > this.size) {
+      return false;
+    }
+    let current = this.head;
+    let previous;
+    let count = 0;
+    if (index === 0) {
+      this.head = current.next;
+    } else {
+      while (count < index) {
+        count++;
+        previous = current;
+        current = current.next;
+      }
+      previous.next = current.next;
+    }
+    this.size--;
   }
 
   toArray() {
@@ -44,14 +97,15 @@ class List {
   }
 }
 
-function init() {
-  const list = new List();
-  list.addNode('1').addNode('2').addNode('3');
-  list.addNode('4', '1');
-  console.log(list.toArray());
-  console.log(typeof list);
+const list = new List();
+list.addNode(1);
+list.addNode(2);
+list.addNode(4);
+list.addNode(5, 0);
+list.addNode(6, 2);
+list.addNode(7, 10);
+list.addNode(8);
 
-  console.log(list.print());
-}
+list.removeNode(1);
+console.log(list.print());
 
-init();
